dd484c99a95946d0277dc528d6dde204c51847ad
juice1000 <julienlook@gmx.de>
2023-12-27 14:02:32 +0800

    scroll bottom >= scroll height instead of ===

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -23,12 +23,7 @@ export default function LandingPage({
   const handleScroll = () => {
     const current = divRef.current;
     if (current) {
-      console.log(
-        current.scrollTop + window.innerHeight === current.scrollHeight,
-      );
-      console.log(current.scrollTop, window.innerHeight, current.scrollHeight);
-
-      if (current.scrollTop + window.innerHeight === current.scrollHeight) {
+      if (current.scrollTop + window.innerHeight >= current.scrollHeight) {
         onBottom(true);
       } else {
         onBottom(false);
