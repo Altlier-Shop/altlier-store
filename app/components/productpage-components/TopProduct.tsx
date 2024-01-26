fab8f629de6c02cfe592a500fafb87cf144510c2
hello@altlier.co <hello@altlier.co>
2024-01-27 04:32:22 +0800

    crypto

--- app/components/productpage-components/TopProduct.tsx
+++ app/components/productpage-components/TopProduct.tsx
@@ -132,7 +132,7 @@ export default function TopProduct(props: TopProductProps) {
             </CartForm>
           </div>
           <div className="mt-4 text-sm text-root-tertiary italic flex justify-center">
-            Pay securely with Apple Pay & Paypal. For crypto payments please contact hello@alilier.co
+            Pay securely with Apple Pay & Paypal.
           </div>
         </div>
       </div>
