243d0362747f10f483aa7adb3145d59a24bf88c3
SaurabhBhatiaodesk <s.bhatiaodesk@gmail.com>
2024-04-04 19:28:53 +0530

    order webhook

--- server.ts
+++ server.ts
@@ -20,32 +20,7 @@ import {
  */
 
 
-// async function registerWebhook() {
-//   console.log("ghjhghjk");
 
-//   const myHeaders = new Headers();
-//   myHeaders.append("X-Shopify-Access-Token", "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");
-//   myHeaders.append("Content-Type", "application/json");
-
-//   const raw = JSON.stringify({
-//     "webhook": {
-//       "address": "https://ec69-2405-201-5023-4005-4519-73bb-db6e-b779.ngrok-free.app/api/orderCreate",
-//       "topic": "orders/create",
-//       "format": "json"
-//     }
-//   });
-
-//   const requestOptions = {
-//     method: "POST",
-//     headers: myHeaders,
-//     body: raw,
-//     redirect: "follow"
-//   };
-
-//   const response = await fetch("https://a29f5a-2.myshopify.com/admin/api/2024-04/webhooks.json", requestOptions);
-//   const result = await response.text();
-//   console.log(result);
-// }
 
 
 export default {
@@ -124,7 +99,6 @@ export default {
   },
 };
 
-// await registerWebhook();
 /**
  * This is a custom session implementation for your Hydrogen shop.
  * Feel free to customize it to your needs, add helper methods, or
